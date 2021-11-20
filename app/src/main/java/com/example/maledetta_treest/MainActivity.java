a6f1202e801d0a62781039025e2851b6f62bc806
tirannosario <glt.rosario@gmail.com>
2021-11-21 00:29:56 +0100

    definizione parziale della UI dell'activity dove si mostrano le bacheche e del profilo utente

--- app/src/main/java/com/example/maledetta_treest/MainActivity.java
+++ app/src/main/java/com/example/maledetta_treest/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.maledetta_treest;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+    }
+
+    public void userBtnClicked(View view){
+        Intent intent = new Intent(this, ProfileActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/maledetta_treest/ProfileActivity.java
@@ -0,0 +1,19 @@
+package com.example.maledetta_treest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+
+    public void dismissBtnClicked(View view){
+        this.finish();
+    }
+}
